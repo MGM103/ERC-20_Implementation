17726f282b078760d028617120878de1dde955e0
MGM103 <marcus.marinelli3@gmail.com>
2024-03-04 21:40:51 +1100

    Adding unit tests

---
+++ script/DeployManualERC20.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.21;
+
+import {Script, console} from "forge-std/Script.sol";
+import {ManualERC20} from "../src/ManualERC-20.sol";
+
+contract DeployManualERC20 is Script {
+    function run() external returns (ManualERC20) {
+        vm.startBroadcast();
+        ManualERC20 manualErc20 = new ManualERC20();
+        vm.stopBroadcast();
+        return manualErc20;
+    }
+}
---
+++ test/unit/ManualERC-20Test.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.21;
+
+import {Test, console} from "forge-std/Test.sol";
+import {ManualERC20} from "../../src/ManualERC-20.sol";
+import {DeployManualERC20} from "../../script/DeployManualERC20.sol";
+
+contract ManualERC20UnitTest is Test {
+    ManualERC20 manualErc20;
+
+    function setUp() external {
+        DeployManualERC20 deployManualErc20 = new DeployManualERC20();
+        manualErc20 = deployManualErc20.run();
+    }
+
+    function testName() public view {
+        assert(
+            keccak256(abi.encodePacked(manualErc20.name())) ==
+                keccak256(abi.encodePacked("Manual ERC-20"))
+        );
+    }
+
+    function testSymbol() public view {
+        assert(
+            keccak256(abi.encodePacked(manualErc20.symbol())) ==
+                keccak256(abi.encodePacked("MERC20"))
+        );
+    }
+
+    function testDecimals() public view {
+        assert(manualErc20.decimals() == 18);
+    }
+
+    function testTotalSupply() public view {
+        assert(manualErc20.totalSupply() == 1000000 ether);
+    }
+}
